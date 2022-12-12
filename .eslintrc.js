57660cf7e4f5f7f13e11c0f13de0ecf01fc8f754
IhorRymar <95086464+IhorRymar@users.noreply.github.com>
2022-12-12 09:36:37 +0200

    Revert "fixed eslint"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,22 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ['standard', 'prettier'],
-  parserOptions: {
-    ecmaVersion: 12,
-  },
-  rules: {},
-}
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
